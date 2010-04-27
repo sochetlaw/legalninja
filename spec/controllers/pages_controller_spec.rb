a81bff77a782f33e2807b295fb339b827c1f927a
joe <jsochet@sochetlaw.com>
2010-04-27 13:13:17 -0400

    Done with static pages

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe PagesController do
+  integrate_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+                               "LegalNinja | Home")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title",
+                               "LegalNinja | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title",
+                               "LegalNinja | About")
+    end
+  end
+end
+
