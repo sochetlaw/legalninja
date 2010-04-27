449404c0abe3edd843e7b361db7634ab0461c490
joe <jsochet@sochetlaw.com>
2010-04-27 15:13:41 -0400

    Removed default Raisl Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  #returns the title on a per-page basis.
+  def title
+    base_title = "LegalNinja"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  
+  map.about   '/about',   :controller => 'pages', :action => 'about'
+  
+  map.help    '/help',    :controller => 'pages', :action => 'help'
+
+  map.root :controller => 'pages', :action => 'home'
+  
   map.resources :microposts
 
   map.resources :microposts
